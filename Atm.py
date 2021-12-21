15fa2a33e02f4caa4cabaf7e967fac30c6a7f1dc
christinawondwessen <78184033+christinawondwessen@users.noreply.github.com>
2021-12-21 11:41:34 -0500

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,28 @@
+import atm
+
+class Atm:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def __init__(self, cash_withdrawl):
+        self.cash_withdrawl = cash_withdrawl
+
+    def __init__(self, balance_enquiry):
+        self.balance_enquiry = balance_enquiry
+        
+    def insert_pin(self, pin_from, pin_to):
+        """Insert Pin here"""
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+
+def main():
+
+file_from = input("Enter the file path to transfer : -") 
+file_to = input("enter the full path to upload to dropbox:- ")
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
+
+if __name__ == '__main__':
+    main()
